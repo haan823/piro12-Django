985fc20017eba6e44efb1139ef9edc4662475b17
sanghyun <bejimil@g.skku.edu>
2020-01-20 14:43:19 +0900

    14강까지

--- askcompany/askcompany/shop/models.py
+++ askcompany/askcompany/shop/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -7,3 +8,33 @@ class Item(models.Model):
     price = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_publish = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'<{self.pk}> {self.name}'
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    blog_url = models.URLField(blank=True)
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(allow_unicode=True, db_index=True)
+    desc = models.TextField(blank=True)
+    image = models.ImageField(blank=True)
+    comment_count = models.PositiveIntegerField(default=0)
+    tag_set = models.ManyToManyField('Tag', blank=True)
+    is_publish = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)

2a5a5c39e86b8a280f5ad7abbbbb3aab97b46cdb
sanghyun <bejimil@g.skku.edu>
2020-01-22 12:13:06 +0900

    중급 기본인증 12강까지

--- askcompany2/askcompany2/accounts/views.py
+++ askcompany2/askcompany2/accounts/views.py
@@ -1,31 +1,58 @@
 from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.views.generic import CreateView
-from django.shortcuts import render, redirect
+from django.contrib.auth.views import PasswordChangeView
 
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.shortcuts import render, redirect, resolve_url
+from .forms import SignupForm
 
 # Create your views here.
 '''
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
-            return redirect(settings.LOGIN_URL)
+            auth_login(request, user) # 로그인 처리
+            next_url = request.GET.get('next') or 'profile'
+            return redirect(next_url)
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'accounts/signup.html', {
         'form': form,
     })
 '''
-signup = CreateView.as_view(model=User,
-                            form_class=UserCreationForm,
-                            success_url=settings.LOGIN_URL,
-                            template_name='accounts/signup.html')
+
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
+
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect(self.get_success_url())
+
+signup = SignupView.as_view()
 
 
 @login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
+
+class MyPasswordChangeView(PasswordChangeView):
+    success_url = reverse_lazy('profile')
+    template_name = 'accounts/password_change_form.html'
+
+    def form_valid(self, form):
+        messages.info(self.request, '암호 변경을 완료했습니다.')
+        return super().form_valid(form)
